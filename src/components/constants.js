b4f1b2fd55ac3c318e1cdfdbf4ce05dee5b8b194
Tzzziqi <btysxq@gmail.com>
2023-07-20 00:00:21 -0700

    first commit

---
+++ src/components/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "96SCVF-H9C3JH-G9QKKR-4WFT";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "https://3.21.53.200";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
